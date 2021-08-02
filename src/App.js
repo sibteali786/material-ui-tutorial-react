e3e1939e4c9cf7c0a1259c64746c884b5408f769
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-08-02 11:39:45 +0500

    Added textfiled and checkox material ui component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import { Button } from "@material-ui/core";
 import { ButtonGroup } from "@material-ui/core";
-import { Checkbox } from "@material-ui/core";
+import { Checkbox, FormControlLabel, TextField } from "@material-ui/core";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
 
@@ -17,6 +17,18 @@ function CheckboxExample() {
          
       /> */}
       {/* for applying label to check box use formcontroller */}
+      <FormControlLabel
+        label="Hello Buddy"
+        control={
+          <Checkbox
+            checked={checked}
+            icon={<DeleteIcon />} // using delete icon and save icon instead of tick box
+            checkedIcon={<SaveIcon />}
+            onChange={(e) => setChecked(e.target.checked)}
+            color="primary"
+          />
+        }
+      />
     </div>
   );
 }
@@ -28,6 +40,15 @@ function App() {
       {/* We can directly use href in button to make it a link. Like href="#" */}
       <header className="App-header">
         <CheckboxExample />
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          color="secondary"
+          type="email"
+          label="The Time"
+          // value="test@test.com"
+          placeholder="test@test.com"
+        />
         <ButtonGroup variant="outlined" color="secondary" aria-label="">
           <Button endIcon={<SaveIcon />}>Save</Button>
           <Button endIcon={<DeleteIcon />}>Discard</Button>
