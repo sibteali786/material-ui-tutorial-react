2424d94bfac28f1cf43a4491de4882d45c3519e2
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-08-02 10:57:01 +0500

    made changes in App.js for material UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import "./App.css";
+import React from "react";
 import { Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import Icon from "@material-ui/core/Icon";
+import { ButtonGroup } from "@material-ui/core";
+import { Checkbox } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
-function App() {
-  const classes = useStyles();
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
   return (
     <div>
+      {/* <Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        color="primary"
+         
+      /> */}
+      {/* for applying label to check box use formcontroller */}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
       Hello World
-      <Button variant="text" color="secondary">
-        Hello World
-      </Button>
-      <div>
+      {/* We can directly use href in button to make it a link. Like href="#" */}
+      <header className="App-header">
+        <CheckboxExample />
+        <ButtonGroup variant="outlined" color="secondary" aria-label="">
+          <Button endIcon={<SaveIcon />}>Save</Button>
+          <Button endIcon={<DeleteIcon />}>Discard</Button>
+        </ButtonGroup>
+        <Button
+          // startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
+          onClick={() => console.log("Hello")}
+          size="large" // vary its size
+          // disabled // To disable it
+          style={{
+            fontSize: 24, // Custom styling
+          }}
+          variant="contained"
+          color="secondary"
+        >
+          Hello World
+        </Button>
+      </header>
+      {/* <div>
         <Button
           variant="contained"
           color="primary"
@@ -25,7 +56,7 @@ function App() {
         >
           Send
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 }
